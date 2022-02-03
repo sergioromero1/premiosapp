14758b99de30993c463ac3c4f2931366f75a1b3a
Sergio <sar9206@gmail.com>
2022-02-03 15:08:49 -0500

    primeras vistas

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hello word")
+    return HttpResponse("Estas en la pagina principal de premios app")
+
+def detail(request, question_id):
+    return HttpResponse(f'Estas viendo la pregunta número{question_id}')
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta número{question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando a la pregunta número{question_id}')
