e3f00779c5f9260bc69fbabb110b89ec96f2cd6d
Sergio <sar9206@gmail.com>
2022-02-08 13:51:38 -0500

    test detail view

--- premiosapp/polls/tests.py
+++ premiosapp/polls/tests.py
@@ -1,4 +1,5 @@
 import datetime
+from urllib import response
 
 from django.test import TestCase
 from django.utils import timezone
@@ -70,7 +71,7 @@ class QuestionIndexViewTest(TestCase):
         displayed
         """
         past_question = create_question(question_text="Past question", days=-30)
-        furute_question = create_question(question_text="Future question", days=30)
+        future_question = create_question(question_text="Future question", days=30)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_question_list"],[past_question])
 
@@ -93,4 +94,24 @@ class QuestionIndexViewTest(TestCase):
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_question_list"],[])
 
-        
+class QuestionDetailViewTest(TestCase):
+    
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 error not found
+        """
+        future_question = create_question(question_text="Future question", days=30)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past displays the 
+        question's text
+        """
+        past_question = create_question(question_text="past question", days=-30)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
